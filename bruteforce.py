496fb0b37d1650b04d53b2687ad4d4d83ca1a3d9
Entropie67 <olivier.elophe@gmail.com>
2022-03-09 11:24:59 +0100

    Fonction de lecture et de nettoyage + fonction bénéfice

--- bruteforce.py
+++ bruteforce.py
@@ -3,14 +3,30 @@ import time
 
 # Initialisation du chronomètre
 start = time.time()
-
-# Lecture du csv
-with open('data/liste_actions.csv', newline='') as datafile:
-    actions = list(csv.reader(datafile, delimiter=',', quotechar='|'))[1:]
+BUDGET = 500
+FICHIER = 'data/liste_actions.csv'
 
 
-print(actions)
+def lecture(fichier: list[str]) -> list[str, float, float]:
+    """
+        fonction qui va lire dans  un fichier csv et retourne une liste d'action
+    """
+    with open(fichier, newline='') as datafile:
+        actions = list(csv.reader(datafile, delimiter=',', quotechar='|'))[1:]
+    actions = [[a[0], float(a[1]), float(a[2][:-1])/100] for a in actions]
+    return actions
 
+def benefice(portefeuille: list[str, float, float]) -> float:
+    """
+        Fontion qui retourne le bénéfice à partir d'une liste d'action (portefeuille)
+    """
+    s = 0
+    for action in portefeuille:
+        s += action[1] * action[2]
+    return s
+
+b = lecture(FICHIER)
+print(benefice(b))
 
 
 
