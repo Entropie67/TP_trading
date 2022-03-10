66b20a958782de11bd46354d53f44f78a1353ae5
Entropie67 <olivier.elophe@gmail.com>
2022-03-10 11:16:03 +0100

    Début de l'optimisation

--- optimized.py
+++ optimized.py
@@ -1,3 +1,18 @@
 import time
 from config import BUDGET, ACTIONS, ACTIONS_1, ACTIONS_2
 
+################################
+#          Algo Glouton        #
+################################
+
+
+
+
+
+
+
+
+
+#######################################
+#      Programmation dynamique        #
+#######################################
