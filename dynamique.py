0a671fe621d7769381b9d9e0d07e0ff264201d7c
Entropie67 <olivier.elophe@gmail.com>
2022-03-11 09:30:01 +0100

    correction poids

--- dynamique.py
+++ dynamique.py
@@ -10,7 +10,7 @@ def dynamique1(act, poids_max=BUDGET):
     act.sort(key=lambda x: x[1])
     valeurs = [a[1] * a[2] for a in act]
     # Conversion des points en int en multipliant par 100
-    poids = [int(a[2] * 100) for a in act]
+    poids = [int(a[1] * 100) for a in act]
 
     # Création d'une matrice n ligne et poids_max colonnes
     v = [[0] * (poids_max + 1) for i in range(n)]
