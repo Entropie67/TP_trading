529b3efbc525b34b9b2422371cfd020ff97359d0
Entropie67 <olivier.elophe@gmail.com>
2022-03-11 09:28:44 +0100

    début programtion dynamique

--- dynamique.py
+++ dynamique.py
@@ -1,15 +1,20 @@
-from config import BUDGET
+from config import BUDGET, ACTIONS
 
 def recursive():
     return 0
 
 # version 1
-def dynaique1(act, poids_max=BUDGET):
+def dynamique1(act, poids_max=BUDGET):
     n = len(act)
+    # On range les actions par cout croissant.
+    act.sort(key=lambda x: x[1])
     valeurs = [a[1] * a[2] for a in act]
     # Conversion des points en int en multipliant par 100
     poids = [int(a[2] * 100) for a in act]
-    actions = [ [act[0], ] for a in act]
+
     # Création d'une matrice n ligne et poids_max colonnes
     v = [[0] * (poids_max + 1) for i in range(n)]
-    for p in range(actions)
+    print(act)
+    print(poids)
+
+dynamique1(ACTIONS)
